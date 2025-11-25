2148842d47a655b157677a9ac5e7af85b7a6bf27
SarahSu92 <sarah.sundberg@outlook.com>
2025-11-25 13:43:54 +0100

    (build): isntalled react-router

---
+++ AtlantisFrame/src/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router';
+
+
+export const HomePage = () => {
+    return (
+        <>
+         <div className="herosection">
+        <img
+          className="heroimage"
+          src={`${import.meta.env.BASE_URL}zoo-animals.jpg`}
+          alt="Djurpark med apor, giraffer och lejon."
+        />
+        <h1 className="heroh1">Välkommen till Zoo sidan</h1>
+      </div>
+      <div className="heroinfo">
+        <Link to={`/Animals`} className="herolink">
+        <h2 className='herolink'>Besök vår djursida för att se alla djur!</h2>
+        </Link>
+      </div>
+        </>
+    );
+};
